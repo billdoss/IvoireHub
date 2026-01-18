10c64bd10e7c07005606882f71f11d37339e620b
Abdoul Dosso <abdouldosso78@gmail.com>
2026-01-18 00:51:23 +0000

    ddd

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  /*server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
-  },
+  },*/
   base: "/IvoireHub/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
